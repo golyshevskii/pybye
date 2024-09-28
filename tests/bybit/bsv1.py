0c7b804398b5b4eb4d154fa29df2dda6395330bd
Slava Golyshevskii <sgolyshevskii@gmail.com>
2024-09-28 21:27:53 +0400

    Backtesting BS v2

--- tests/bybit/bsv1.py
+++ tests/bybit/bsv1.py
@@ -1,40 +1,11 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 from backtesting import Backtest
 from config import BYBIT_DATA_PATH
 from core.scripts.tools.logger import get_logger
-from tests.tools.backtest import BreakoutStrategy
+from tests.tools.breakout import BreakoutStrategyV1
+from tests.tools.optimizer import optimize
 
 logger = get_logger(__name__)
-
-
-def optimize(bt: Backtest):
-    """
-    Optimize the TP and SL percentages
-
-    Params:
-        bt: The Backtest object
-    """
-    optimizer, heatmap = bt.optimize(
-        TP_PERCENTAGE=range(1, 10, 1),
-        SL_PERCENTAGE=range(1, 5, 1),
-        maximize="Equity Final [$]",
-        return_heatmap=True,
-    )
-    logger.info(f"\n\n{'='*20} STATS {'='*20}\n{optimizer}\n\n{'='*20} SL & TP {'='*20}\n{heatmap}\n\n")
-
-    heatmap = heatmap.reset_index(name="Equity_Final")
-    heatmap = heatmap.pivot(index="TP_PERCENTAGE", columns="SL_PERCENTAGE", values="Equity_Final")
-
-    # Create the heatmap
-    plt.figure(figsize=(10, 6))
-    sns.heatmap(heatmap, annot=True, fmt=".2f", cmap="coolwarm", cbar_kws={"label": "Equity Final [$]"})
-    plt.title("Equity Final Heatmap for TP and SL Percentages")
-    plt.xlabel("SL Percentage")
-    plt.ylabel("TP Percentage")
-    plt.show()
-
 
 if __name__ == "__main__":
     import argparse
@@ -62,7 +33,7 @@ if __name__ == "__main__":
     H_kline.columns = ["Open", "High", "Low", "Close", "Volume"]
 
     # Run the backtest
-    bs = BreakoutStrategy
+    bs = BreakoutStrategyV1
     bs.RL = D_kline["resistance"]
 
     bt = Backtest(H_kline, bs, cash=100, commission=0.01)
